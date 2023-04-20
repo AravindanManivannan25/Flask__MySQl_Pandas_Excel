c1a885ff773e38feb4df190e6062123813a1f9a9
AravindanManivannan25 <aravind25mani@gmail.com>
2023-04-20 12:17:11 +0530

    "sixth_commit"

--- pandas1.py
+++ pandas1.py
@@ -4,13 +4,15 @@ from config import  mysql
 from flask import flash, request ,Response ,render_template , url_for , send_file
 import pandas as pd
 import os
+import datetime
+import random
 #import xlswritter
 
    
 @app.route('/')
 def my_functions():
     try:
-        conn = mysql.connect()
+        conn = mysql.connect( )
 
         """ cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute ('select * from students')
@@ -27,14 +29,17 @@ def my_functions():
         #my_data2=pd.read_sql(query2 , conn)
         print(f"my_data : {my_data}")
 
+        file_name = (f" {datetime.datetime.now().strftime('%Y%m%d_%H%M%S')} + {random.randint(1,1000)}.xlsx")
+        file_path = os.path.join(os.path.dirname(__file__) ,'Excel' , file_name) 
+
         #output ='C:\\Users\\DELL\\Projects\\Flask_MySql_Excel\\Excel\\my_filepandas346.xlsx'
-        file_path = os.path.join(os.path.dirname(__file__) , 'Excel', 'my_filepandas346.xlsx')
+        #file_path = os.path.join(os.path.dirname(__file__) , 'Excel', 'my_filepandas346.xlsx')
         with pd.ExcelWriter(file_path) as output:
             my_data.to_excel(output,sheet_name = 'Full_Details' , index = False)
             my_data1.to_excel(output,sheet_name = 'Age_Above_30', index = False) 
             my_data2.to_excel(output, sheet_name='Age_below_30', index = False)
         
-        print(f"path : {__file__}")
+        print(f"path : {__file__ }")
         return  send_file (output, as_attachment=True, mimetype='application/vnd.ms-excel')
     
         #return f"File downloaded {my_data.to_excel('my_filepanda32.xlsx')}"
